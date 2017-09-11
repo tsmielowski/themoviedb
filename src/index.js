da50a1aeac572b50060ea9a91a88177715ac607a
tsmielowski <smielowski@tlen.pl>
2017-09-11 21:43:24 +0200

    Provide testing input data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import RootComponent from "./components/RootComponent";
+//import registerServiceWorker from './registerServiceWorker';
+const items = [ {
+        id: 1,
+        name: "fake one"
+    }, {
+        id: 2,
+        name: "fake two"
+    }, {
+        id: 3,
+        name: "fake three"
+    }, {
+        id: 4,
+        name: "fake four"
+    } ];
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render( <RootComponent items={items} />, document.getElementById( "root" ) );
+//registerServiceWorker();
